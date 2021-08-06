051b7c3e0069cfc481ec167def99fcaeb4bd2b68
Daniel Velasquez <dvn228@nyu.edu>
2021-08-06 12:58:44 -0400

    Made the `SingleTxn` and `MergedTxn` classes, rendering the code more object-oriented and strongly typed

--- sort_and_match.py
+++ sort_and_match.py
@@ -1,12 +1,13 @@
-from operator import itemgetter
+from operator import attrgetter
 from datetime import datetime as dt
-from typing import List, Dict
+from typing import Union, List, Dict
 from re import Pattern
 from enum import Enum
 import re
 
 ##### CHASE ##################################################################
 CH_FILE = './in/chase.csv'
+CH_START_BAL = 362335
 
 # INDEX = 1            2                         3                  4                        5         6
 # COLS =  debit/credit date                      title              amt                      type      balance
@@ -16,6 +17,7 @@ CH_REGEX = re.compile(
 
 ##### GOODBUDGET #############################################################
 GB_FILE = './in/goodbudget.csv'
+GB_START_BAL = 0
 
 # INDEX = 1                         2                                                   3                                  4                       5                                           6      7
 # COLS =  date                      envelope                            account         title                              notes                   amt                                         status details
@@ -35,18 +37,68 @@ MATCHED_FILE = './out/matched/matched.csv'
 ##############################################################################
 
 
-def read_txns(file_name: str, regex: Pattern):
-    txns = []
+# Types
+class TxnType(Enum):
+    CHASE = 'ch'
+    GOODBUDGET = 'gb'
+    BOTH = 'both'
+
+
+class SingleTxn:
+    def __init__(self, ts: int, date: str, title: str, amt: int):
+        self.ts = ts
+        self.date = date
+        self.title = title
+        self.amt = amt
+        self.bal = -1
+
+    def to_row(self) -> str:
+        return ','.join([str(value) for key, value in vars(self).items() if key != 'ts'])
+
+
+class MergedTxn:
+    def __init__(self, txn_group: Dict[TxnType, SingleTxn]):
+        if TxnType.CHASE in txn_group and TxnType.GOODBUDGET in txn_group:
+            self.type_ = TxnType.BOTH
+        else:
+            self.type_ = list(txn_group)[0]
+
+        if TxnType.CHASE in txn_group:
+            self.ch_txn = txn_group[TxnType.CHASE]
+        if TxnType.GOODBUDGET in txn_group:
+            self.gb_txn = txn_group[TxnType.GOODBUDGET]
+
+        self.special = False
+
+    def get_ts(self) -> int:
+        if self.type_ in [TxnType.BOTH, TxnType.CHASE]:
+            return self.ch_txn.ts
+        else:
+            return self.gb_txn.ts
+
+    def to_row(self) -> str:
+        ch_row = self.ch_txn.to_row() \
+            if hasattr(self, 'ch_txn') else ','.join(['' for _ in range(len(vars(self.gb_txn))-1)])
+        gb_row = self.gb_txn.to_row() \
+            if hasattr(self, 'gb_txn') else ','.join(['' for _ in range(len(vars(self.ch_txn))-1)])
+
+        return ','.join([self.type_.name, ch_row, gb_row, str(self.special)])
+
+
+def read_txns(file_name: str, regex: Pattern) -> List[SingleTxn]:
+    txns: List[SingleTxn] = []
     with open(file_name) as in_file:
         amt_unmatched = 0
         for line in in_file:
             if (txn := regex.match(line)) and (txn := txn.groupdict()):
-                txns.append({
-                    'ts': dt.strptime(txn['date'], "%m/%d/%Y").timestamp(),
+                txn_amt = int(txn['amt']
+                              .replace('"', '').replace(",", '').replace(".", ''))
+                txns.append(SingleTxn(**{
+                    'ts': int(dt.strptime(txn['date'], "%m/%d/%Y").timestamp()),
                     'date': txn['date'],
                     'title':  re.sub(r'\s+', ' ', txn['title']),
-                    'amt': txn['amt'].replace('"', '').replace(",", '').replace(".", '')
-                })
+                    'amt': txn_amt,
+                }))
             else:
                 amt_unmatched += 1
                 print(f'No match: {line}', end='')
@@ -56,94 +108,35 @@ def read_txns(file_name: str, regex: Pattern):
     return txns
 
 
-def sort_txns(txns: List[Dict]):
-    # make 'amt' a int to allow numeric sorting
-    txns = [dict(txn, amt=int(txn['amt'])) for txn in txns]
-
-    # sort
-    txns = sorted(txns, key=itemgetter('amt', 'ts', 'title'))
-
-    # make 'amt' a string again
-    return [dict(txn, amt=str(txn['amt'])) for txn in txns]
-
-
-def dict_vals(a_dict: Dict[str, str]) -> List[str]:
-    return [value for key, value in a_dict.items() if 'ts' not in key]
-
-
-def txns_to_file(file_name: str, txns: List) -> None:
+def txns_to_file(file_name: str, txns: Union[List[SingleTxn], List[MergedTxn]]) -> None:
     with open(file_name, 'w') as out_file:
         for txn in txns:
-            out_file.write(f"{','.join(dict_vals(txn))}\n")
-
-
-class TxnType(Enum):
-    CHASE = 'ch'
-    GOODBUDGET = 'gb'
-    BOTH = 'both'
-
-
-MERGED_COLS = {
-    'type': None,
-    'ch_ts': -1.0,
-    'ch_date': '',
-    'ch_title': '',
-    'ch_amt': '',
-    'ch_bal': '',
-    'gb_ts': -1.0,
-    'gb_date': '',
-    'gb_title': '',
-    'gb_amt': '',
-    'gb_bal': '',
-    'special': False
-}
-
-
-def txn_to_row(txn_group: Dict[TxnType, Dict]):
-    row = MERGED_COLS.copy()
-
-    if TxnType.CHASE in txn_group and TxnType.GOODBUDGET in txn_group:
-        row['type'] = TxnType.BOTH
-    else:
-        row['type'] = list(txn_group)[0]
-
-    for txn_type, txn in txn_group.items():
-        for key, value in txn.items():
-            row[f'{txn_type.value}_{key}'] = value
-
-    return row
-
-
-def get_row_ts(row: Dict):
-    if row['type'] in [TxnType.BOTH, TxnType.CHASE]:
-        return row['ch_ts']
-    else:
-        return row['gb_ts']
+            out_file.write(f"{txn.to_row()}\n")
 
 
 # read
-ch_txns = read_txns(CH_FILE, CH_REGEX)
-gb_txns = read_txns(GB_FILE, GB_REGEX)
+ch_txns: List[SingleTxn] = read_txns(CH_FILE, CH_REGEX)
+gb_txns: List[SingleTxn] = read_txns(GB_FILE, GB_REGEX)
 
-# sort
-ch_txns = sort_txns(ch_txns)
-gb_txns = sort_txns(gb_txns)
+# sort by amount
+ch_txns = sorted(ch_txns, key=attrgetter('amt', 'ts', 'title'))
+gb_txns = sorted(gb_txns, key=attrgetter('amt', 'ts', 'title'))
 
 # print sorted txns to file for debugging
 txns_to_file(CH_SORTED_FILE, ch_txns)
 txns_to_file(GB_SORTED_FILE, gb_txns)
 
 # merge chase txns to gb txns
-merged_txns = []
+merged_txns: List[MergedTxn] = []
 ch_i, gb_i = 0, 0
 while ch_i < len(ch_txns) or gb_i < len(gb_txns):
     ch_txn = ch_txns[ch_i] if ch_i < len(ch_txns) else None
     gb_txn = gb_txns[gb_i] if gb_i < len(gb_txns) else None
 
-    ch_amt = int(ch_txn['amt']) if ch_txn else None
-    gb_amt = int(gb_txn['amt']) if gb_txn else None
+    ch_amt = ch_txn.amt if ch_txn else None
+    gb_amt = gb_txn.amt if gb_txn else None
 
-    amt_days_diff = ((int(gb_txn['ts']) - int(ch_txn['ts'])) / (60 * 60 * 24))  \
+    amt_days_diff = ((gb_txn.ts - ch_txn.ts) / (60 * 60 * 24)) \
         if ch_txn and gb_txn else None
 
     merged_txn = {}
@@ -157,26 +150,22 @@ while ch_i < len(ch_txns) or gb_i < len(gb_txns):
             merged_txn[TxnType.GOODBUDGET] = gb_txn
             gb_i += 1
 
-    merged_txns.append(txn_to_row(merged_txn))
+    merged_txns.append(MergedTxn(merged_txn))
 
 # sort by date
-merged_txns = sorted(merged_txns, key=get_row_ts)
+merged_txns = sorted(merged_txns, key=lambda x: x.get_ts())
 
-# set balances
-ch_bal, gb_bal = 362335, 0
-for txn in merged_txns:
-    if txn['type'] in [TxnType.CHASE, TxnType.BOTH]:
-        ch_bal += int(txn['ch_amt'])
-        txn['ch_bal'] = str(ch_bal)
-    if txn['type'] in [TxnType.GOODBUDGET, TxnType.BOTH]:
-        gb_bal += int(txn['gb_amt'])
-        txn['gb_bal'] = str(gb_bal)
+# set balances and mark when balances are equal to eachother
+ch_bal, gb_bal = CH_START_BAL, GB_START_BAL
+for merged_txn in merged_txns:
+    if merged_txn.type_ in [TxnType.CHASE, TxnType.BOTH]:
+        ch_bal += merged_txn.ch_txn.amt
+        merged_txn.ch_txn.bal = ch_bal
+    if merged_txn.type_ in [TxnType.GOODBUDGET, TxnType.BOTH]:
+        gb_bal += merged_txn.gb_txn.amt
+        merged_txn.gb_txn.bal = gb_bal
     if ch_bal == gb_bal:
-        txn['special'] = True
-
-# make 'type' and 'special' strings to allow printing to file
-merged_txns = [dict(txn, type=txn['type'].name,
-                    special=str(txn['special'])) for txn in merged_txns]
+        merged_txn.special = True
 
 # print to file
 txns_to_file(MATCHED_FILE, merged_txns)
