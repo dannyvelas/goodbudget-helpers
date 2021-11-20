10bf8605d93df4a61c62effac0a40fefc459b335
Daniel Velasquez <dvn228@nyu.edu>
2021-11-20 16:39:35 -0500

    created a Config() class for easier ENV variable access

--- config.py
+++ config.py
@@ -10,27 +10,22 @@ def _str_to_int(s: str) -> int:
         return 0
 
 
-def get_ch_start_bal(env: Dict[str, Union[str, None]]) -> int:
-    env_ch_start_bal = env["CH_START_BAL"] if "CH_START_BAL" in env and env["CH_START_BAL"] is not None else ""
-    return _str_to_int(env_ch_start_bal)
+class Config:
+    def __init__(self, env: Dict[str, Union[str, None]]):
+        ch_start_bal = env["CH_START_BAL"] if "CH_START_BAL" in env and env["CH_START_BAL"] is not None else ""
+        gb_start_bal = env["GB_START_BAL"] if "GB_START_BAL" in env and env["GB_START_BAL"] is not None else ""
 
+        self.ch_start_bal = _str_to_int(ch_start_bal)
+        self.gb_start_bal = _str_to_int(gb_start_bal)
 
-def get_gb_start_bal(env: Dict[str, Union[str, None]]) -> int:
-    env_gb_start_bal = env["GB_START_BAL"] if "GB_START_BAL" in env and env["GB_START_BAL"] is not None else ""
-    return _str_to_int(env_gb_start_bal)
+        if "GB_USERNAME" in env and env["GB_USERNAME"] is not None:
+            self.gb_username = env["GB_USERNAME"]
+        else:
+            print("Warning: GB_USERNAME not found in environment")
+            self.gb_username = ""
 
-
-def get_gb_username(env: Dict[str, Union[str, None]]) -> str:
-    if "GB_USERNAME" in env and env["GB_USERNAME"] is not None:
-        return env["GB_USERNAME"]
-    else:
-        print("Warning: GB_USERNAME not found in environment")
-        return ""
-
-
-def get_gb_password(env: Dict[str, Union[str, None]]) -> str:
-    if "GB_PASSWORD" in env and env["GB_PASSWORD"] is not None:
-        return env["GB_PASSWORD"]
-    else:
-        print("Warning: GB_PASSWORD not found in environment")
-        return ""
+        if "GB_PASSWORD" in env and env["GB_PASSWORD"] is not None:
+            self.gb_password = env["GB_PASSWORD"]
+        else:
+            print("Warning: GB_PASSWORD not found in environment")
+            self.gb_password = ""
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 from dotenv import dotenv_values
 
 from add_new_txns import add_new_txns
-from config import get_ch_start_bal, get_gb_password, get_gb_start_bal, get_gb_username
+from config import Config
 from file_in import read_ch_txns, read_gb_txns
 from file_out import OUT_DIR, write_txns_grouped_to_files
 from match import get_txns_grouped
@@ -13,10 +13,9 @@ if not ENV:
     print("Error, no .env file found.")
     exit(1)
 
-CH_START_BAL = get_ch_start_bal(ENV)
-GB_START_BAL = get_gb_start_bal(ENV)
-MAX_DAYS_APART = 7
+config = Config(ENV)
 
+MAX_DAYS_APART = 7
 add_txns = False
 graph = False
 
@@ -36,7 +35,7 @@ ch_txns = read_ch_txns()
 gb_txns = read_gb_txns()
 
 txns_grouped = get_txns_grouped(
-    ch_txns, gb_txns, CH_START_BAL, GB_START_BAL, MAX_DAYS_APART)
+    ch_txns, gb_txns, config.ch_start_bal, config.gb_start_bal, MAX_DAYS_APART)
 
 # print some helpful numbers
 print(f'AMT OF UNMATCHED CHASE TXNS: {len(txns_grouped.only_ch_txns)}')
@@ -53,12 +52,10 @@ if add_txns:
         print("Error, no .envelopes.env file found.")
         exit(1)
 
-    GB_USERNAME = get_gb_username(ENV)
-    GB_PASSWORD = get_gb_password(ENV)
     last_gb_txn_ts = gb_txns[-1].ts if len(gb_txns) > 0 else 0
 
-    add_new_txns(txns_grouped, ENVELOPES, GB_USERNAME,
-                 GB_PASSWORD, last_gb_txn_ts)
+    add_new_txns(txns_grouped, ENVELOPES, config.gb_username,
+                 config.gb_password, last_gb_txn_ts)
 
     # print amts
     amt_pending = 0
