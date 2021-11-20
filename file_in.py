94b46beb76fcadaebb2b6334dea2948c7c702e6b
Daniel Velasquez <dvn228@nyu.edu>
2021-11-20 16:14:33 -0500

    moved regexes to their own file

--- file_in.py
+++ file_in.py
@@ -1,19 +1,12 @@
 from datetime import datetime as dt
 import re
 from typing import List
+from regex import CH_REGEX, GB_INCOME_REGEX, GB_EXPENSE_REGEX
 
 from datatypes import ChaseTxn, GoodbudgetTxn
 
 IN_CH_FILE = './in/chase.csv'
 IN_GB_FILE = './in/goodbudget.csv'
-
-# COLUMNS =      debit/credit                  date                        title              amt                      type    balance
-CH_REGEX_STR = r'(?P<deb_or_cred>DEBIT|CREDIT),(?P<date>\d\d\/\d\d\/\d{4}),(?P<title>"[^"]+"),(?P<amt>-?\d{1,4}\.\d\d),[A-Z_]+,(?P<balance>-?\d{1,5}.\d\d| ),,'
-CH_REGEX = re.compile(CH_REGEX_STR)
-
-# COLUMNS =        date                        envelope                                        account         title                                notes                     amt                                         status details
-GB_REGEX_STR = r"""(?P<date>\d\d\/\d\d\/\d{4}),(?P<envelope>"[^"]+"|[A-Za-z]*|\[Unallocated\]),"Chase Account",(?P<title>"[^"]+"|[A-Za-z0-9\.'’-]+),("[^"]+"|[A-Za-z\.'’-]*),,(?P<amt>-?\d{1,3}\.\d\d|"-?\d,\d{3}\.\d\d"),(CLR)?,(((\[Unallocated\]|[A-Za-z]+)\|-?\d{1,3}.\d\d)|("[^"]+"))?"""
-GB_REGEX = re.compile(GB_REGEX_STR)
 
 
 def _shorten(string: str) -> str:
@@ -31,11 +24,12 @@ def read_ch_txns() -> List[ChaseTxn]:
     with open(IN_CH_FILE) as in_file:
         amt_unmatched = 0
         for line in in_file:
-            if (txn := CH_REGEX.match(line)) and (txn := txn.groupdict()):
+            if (txn := CH_REGEX.match(line)):
+                txn = txn.groupdict()
                 txns.append(ChaseTxn(**{
-                    '_ts': int(dt.strptime(txn['date'], "%m/%d/%Y").timestamp()),
-                    '_is_debit': txn['deb_or_cred'] == 'DEBIT',
-                    '_is_pending': txn['balance'] == ' ',
+                    'ts': int(dt.strptime(txn['date'], "%m/%d/%Y").timestamp()),
+                    'is_debit': txn['deb_or_cred'] == 'DEBIT',
+                    'is_pending': txn['balance'] == ' ',
                     'date': txn['date'],
                     'title':  _shorten(txn['title']),
                     'amt': int(txn['amt'].replace('"', '').replace(",", '').replace(".", ''))
@@ -54,9 +48,10 @@ def read_gb_txns() -> List[GoodbudgetTxn]:
     with open(IN_GB_FILE) as in_file:
         amt_unmatched = 0
         for line in in_file:
-            if (txn := GB_REGEX.match(line)) and (txn := txn.groupdict()):
+            if (txn := GB_EXPENSE_REGEX.match(line)) or (txn := GB_INCOME_REGEX.match(line)):
+                txn = txn.groupdict()
                 txns.append(GoodbudgetTxn(**{
-                    '_ts': int(dt.strptime(txn['date'], "%m/%d/%Y").timestamp()),
+                    'ts': int(dt.strptime(txn['date'], "%m/%d/%Y").timestamp()),
                     'date': txn['date'],
                     'title':  _shorten(txn['title']),
                     'envelope': txn['envelope'],
---
+++ regex.py
@@ -0,0 +1,35 @@
+import re
+
+_CH_REGEX_STR = (r'(?P<deb_or_cred>DEBIT|CREDIT)'  # DEB_OR_CRED
+                 r',(?P<date>\d\d\/\d\d\/\d{4})'   # DATE
+                 r',(?P<title>"[^"]+")'            # TITLE
+                 r',(?P<amt>-?\d+\.\d\d)'          # AMT
+                 r',[A-Z_]+'                       # TYPE
+                 r',(?P<balance>-?\d+.\d\d| )'     # BALANCE
+                 r','                              # CHECK_OR_SPLIT_NUM
+                 r',')                             # TRAILING COMMA
+
+_GB_INCOME_REGEX_STR = (r'(?P<date>\d\d\/\d\d\/\d{4})'  # DATE
+                        r',(?P<envelope>)'              # ENVELOPE
+                        r',"Chase Account"'             # ACCOUNT
+                        r',(?P<title>"[^"]+"|[^,]+)'    # TITLE
+                        r',("[^"]+"|[^,]*)'             # NOTES
+                        r','                            # CHECK_NUM
+                        r',(?P<amt>"[^"]+"|[^,]+)'      # AMT
+                        r',(CLR)?'                      # STATUS
+                        r',("[^"]+"|[^\n]+)')           # DETAILS
+
+_GB_EXPENSE_REGEX_STR = (r'(?P<date>\d\d\/\d\d\/\d{4})'  # DATE
+                         # ENVELOPE
+                         r',(?P<envelope>"[^"]+"|[A-Za-z]+|\[Unallocated\])'
+                         r',"Chase Account"'             # ACCOUNT
+                         r',(?P<title>"[^"]+"|[^,]+)'    # TITLE
+                         r',("[^"]+"|[^,]*)'             # NOTES
+                         r','                            # CHECK_NUM
+                         r',(?P<amt>"[^"]+"|[^,]+)'      # AMT
+                         r',(CLR)?'                      # STATUS
+                         r',')                           # DETAILS
+
+CH_REGEX = re.compile(_CH_REGEX_STR)
+GB_INCOME_REGEX = re.compile(_GB_INCOME_REGEX_STR)
+GB_EXPENSE_REGEX = re.compile(_GB_EXPENSE_REGEX_STR)
